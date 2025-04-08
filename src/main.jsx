feff1f9239c63070c303a0526b8d39939416c2c7
[BK] Anh Túc <husthunter97@gmail.com>
2025-04-08 21:16:29 +0700

    initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,26 @@
-import { ConfigProvider } from "antd";
-import React from "react";
-import { createRoot } from "react-dom/client";
-import { Provider } from "react-redux";
-import { ToastContainer } from "react-toastify";
-import "../src/assets/css/index.css";
-import App from "./App.jsx";
-import { ErrorBoundary } from "./error-boundry.jsx";
-import { store } from "./redux/store.js";
+import { ConfigProvider } from 'antd';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { ToastContainer } from 'react-toastify';
+import '../src/assets/css/index.css';
+import App from './App.jsx';
+import { ErrorBoundary } from './error-boundry.jsx';
+import { store } from './redux/store.js';
 
-createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <ConfigProvider
-        theme={{
-          token: {
-            colorPrimary: "#1677ff",
-          },
-        }}
-      >
-        <ErrorBoundary fallback={<h1>Error</h1>}>
-          <App />
-          <ToastContainer position="top-center" />
-        </ErrorBoundary>
-      </ConfigProvider>
-    </Provider>
-  </React.StrictMode>
+createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: '#1677ff',
+        },
+      }}
+    >
+      <ErrorBoundary fallback={<h1>Error</h1>}>
+        <App />
+        <ToastContainer position="top-center" />
+      </ErrorBoundary>
+    </ConfigProvider>
+  </Provider>,
 );
