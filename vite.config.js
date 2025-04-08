60d8af718f042e445ac54c9707dd39a1ea7e37db
[BK] Anh Túc <husthunter97@gmail.com>
2025-04-08 23:46:52 +0700

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Use relative paths for GitHub Pages/Netlify
+  base: '/', // Use relative paths for GitHub Pages/Netlify
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
